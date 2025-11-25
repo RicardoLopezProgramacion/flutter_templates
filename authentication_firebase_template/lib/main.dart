2b5a15d6e3859a6f68dcab010f30a5cb2a3eb062
ricardo lopez <ricardoingsistemas@gmail.com>
2025-11-25 18:45:53 -0500

    update template-01

--- authentication_firebase_template/lib/main.dart
+++ authentication_firebase_template/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:authentication_firebase_template/firebase_options.dart'; // <--- command in terminal (flutterfire configure --project=back-demo-username)
 import 'package:authentication_firebase_template/screens/home_screen.dart';
 import 'package:authentication_firebase_template/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // <--- command in terminal (flutterfire configure --project=back-demo-username)
   runApp(const MainApp());
 }
 
